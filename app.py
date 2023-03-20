eadb6779ec24f32cdf004f12b55613beb154580b
Subho Ghosh <ighoshsubho@gmail.com>
2023-03-20 18:27:54 +0530

    Removed the comments

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from flask_pymongo import PyMongo
-# from bson.json_util import dumps
-# from bson.objectid import ObjectId
 from flask import jsonify,request, Flask
 from flask import Flask
 import logging as logger
-# from werkzeug.security import generate_password_hash,check_password_hash
 import os
 from dotenv import load_dotenv, find_dotenv
 logger.basicConfig(level="DEBUG")
@@ -12,79 +9,11 @@ logger.basicConfig(level="DEBUG")
 load_dotenv(find_dotenv())
 
 app = Flask(__name__)
-# app.secret_key = "thisissecret"
  
 app.config['MONGO_URI'] = f"mongodb+srv://{os.getenv('MONGO_URI_USER')}:{os.getenv('MONGO_URI_USER_PASSWORD')}@cluster1.v5rmdlf.mongodb.net/?retryWrites=true&w=majority"
  
 mongo = PyMongo(app)
 
-# @app.route('/add',methods=['POST'])
-# def add_user():
-#     _json = request.json
-#     _name = _json['name']
-#     _email = _json['email']
-#     _password = _json['password']
- 
-#     if _name and _email and _password and request.method == "POST":
- 
-#         _hashed_password = generate_password_hash(_password)
- 
-#         id = mongo.cx.db.user.insert_one({'name':_name,'email':_email,'password':_hashed_password})
- 
-#         resp = jsonify("User Added successfully")
- 
-#         resp.status_code = 200
- 
-#         return resp
- 
-#     else:
- 
-#         return not_found()
-    
-# @app.route('/users')
-# def users():
-#     users = mongo.cx.db.user.find({})
-#     resp = dumps(users)
-#     return resp
-
-# @app.route('/user/<id>')
-# def user(id):
-#     user = mongo.cx.db.user.find({'_id':ObjectId(id)})
-#     resp = dumps(user)
-#     return resp
-
-# @app.route('/delete/<id>',methods=['DELETE'])
-# def delete_user(id):
-#     mongo.cx.db.user.delete_one({'_id':ObjectId(id)})
-#     resp = jsonify("User deleted successfully")
-#     resp.status_code = 200
-#     return resp
-
-# @app.route('/update/<id>',methods=['PUT'])
-# def update_user(id):
-#     _id = id
-#     _json = request.json
-#     _name = _json['name']
-#     _email = _json['email']
-#     _password = _json['password']
- 
-#     if _name and _email and _password and _id and request.method == "PUT":
- 
-#         _hashed_password = generate_password_hash(_password)
- 
-#         mongo.cx.db.user.update_one({'_id':ObjectId(_id['$oid']) if '$oid' in _id else ObjectId(_id)},{'$set':{'name':_name,'email':_email,'password:':_hashed_password}})
- 
-#         resp = jsonify("User updated successfully")
- 
-#         resp.status_code = 200
- 
-#         return resp
- 
-#     else:
- 
-#         return not_found()
-
-# @app.errorhandler(404)
 def not_found(error=None):
     message = {
         'status':404,
